195f6f22dcf93ffd56bd491f470597beb6d704ed
Ernest Oliver Casanes <ecasanes@gmail.com>
2017-07-19 11:21:02 +0800

    update: php 6 compatilble code

--- src/Event.php
+++ src/Event.php
@@ -29,7 +29,7 @@ class Event
         return $event;
     }
 
-    public static function create(array $properties, string $calendarId = null)
+    public static function create(array $properties, $calendarId = null)
     {
         $event = new static();
 
@@ -87,12 +87,12 @@ class Event
         array_set($this->googleEvent, $name, $value);
     }
 
-    public function exists(): bool
+    public function exists()
     {
         return $this->id != '';
     }
 
-    public function isAllDayEvent(): bool
+    public function isAllDayEvent()
     {
         return is_null($this->googleEvent['start']['dateTime']);
     }
@@ -109,8 +109,8 @@ class Event
         Carbon $startDateTime = null,
         Carbon $endDateTime = null,
         array $queryParameters = [],
-        string $calendarId = null
-    ): Collection {
+        $calendarId = null
+    ) {
         $googleCalendar = static::getGoogleCalendar($calendarId);
 
         $googleEvents = $googleCalendar->listEvents($startDateTime, $endDateTime, $queryParameters);
@@ -131,7 +131,7 @@ class Event
      *
      * @return \Spatie\GoogleCalendar\Event
      */
-    public static function find($eventId, $calendarId = null): Event
+    public static function find($eventId, $calendarId = null)
     {
         $googleCalendar = static::getGoogleCalendar($calendarId);
 
@@ -140,9 +140,11 @@ class Event
         return static::createFromGoogleCalendarEvent($googleEvent, $calendarId);
     }
 
-    public function save($method = null): Event
+    public function save($method = null)
     {
-        $method = $method ?? ($this->exists() ? 'updateEvent' : 'insertEvent');
+        if($method){
+            $method = $this->exists() ? 'updateEvent' : 'insertEvent';
+        }
 
         $googleCalendar = $this->getGoogleCalendar($this->calendarId);
 
@@ -156,9 +158,13 @@ class Event
     /**
      * @param string $eventId
      */
-    public function delete(string $eventId = null)
+    public function delete($eventId = null)
     {
-        $this->getGoogleCalendar($this->calendarId)->deleteEvent($eventId ?? $this->id);
+        if($eventId){
+            $eventId = $this->id;
+        }
+
+        $this->getGoogleCalendar($this->calendarId)->deleteEvent($eventId);
     }
 
     /**
@@ -168,7 +174,9 @@ class Event
      */
     protected static function getGoogleCalendar($calendarId = null)
     {
-        $calendarId = $calendarId ?? config('laravel-google-calendar.calendar_id');
+        if($calendarId){
+            $calendarId = config('laravel-google-calendar.calendar_id');
+        }
 
         return GoogleCalendarFactory::createForCalendarId($calendarId);
     }
@@ -177,7 +185,7 @@ class Event
      * @param string         $name
      * @param \Carbon\Carbon $date
      */
-    protected function setDateProperty(string $name, Carbon $date)
+    protected function setDateProperty($name, Carbon $date)
     {
         $eventDateTime = new Google_Service_Calendar_EventDateTime();
 
@@ -205,19 +213,23 @@ class Event
         $this->attendees[] = $attendees;
     }
 
-    protected function getFieldName(string $name): string
+    protected function getFieldName($name)
     {
-        return [
-            'name'          => 'summary',
-            'description'   => 'description',
-            'startDate'     => 'start.date',
-            'endDate'       => 'end.date',
-            'startDateTime' => 'start.dateTime',
-            'endDateTime'   => 'end.dateTime',
-        ][$name] ?? $name;
+        if($name){
+            return [
+                'name'          => 'summary',
+                'description'   => 'description',
+                'startDate'     => 'start.date',
+                'endDate'       => 'end.date',
+                'startDateTime' => 'start.dateTime',
+                'endDateTime'   => 'end.dateTime',
+            ][$name];
+        }
+
+        return $name;
     }
 
-    public function getSortDate(): string
+    public function getSortDate()
     {
         if ($this->startDate) {
             return $this->startDate;
